6194e66489cab0174e8202e3a6ed2f441c94158a
Manideep2363 <163991005+Manideep2363@users.noreply.github.com>
2026-07-20 15:34:37 +0530

    practiced_examples

---
+++ 07_20_2026/rw_lock.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+var number = 100
+var mu sync.RWMutex
+
+func reader(id int, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	mu.RLock()
+	defer mu.RUnlock()
+
+	fmt.Println("reader", id, "reads: ", number)
+
+	time.Sleep(2 * time.Second)
+
+}
+
+func main() {
+	var wg sync.WaitGroup
+
+	wg.Add(3)
+	for i := 1; i <= 3; i++ {
+		go reader(i, &wg)
+	}
+	wg.Wait()
+}
