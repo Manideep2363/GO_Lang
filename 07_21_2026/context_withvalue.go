b4a568216fb2c985aab09763c2e2ac22fde06b58
Manideep2363 <163991005+Manideep2363@users.noreply.github.com>
2026-07-21 10:36:30 +0530

    inital-commit

---
+++ 07_21_2026/context_withvalue.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"context"
+	"fmt"
+)
+
+type contextKey string
+
+const userIDKey contextKey = "userID"
+
+func process(ctx context.Context) {
+	userId := ctx.Value(userIDKey)
+	fmt.Println(userId)
+
+}
+
+func main() {
+
+	parentCtx := context.Background()
+	ctx := context.WithValue(parentCtx, userIDKey, 101)
+
+	process(ctx)
+}
